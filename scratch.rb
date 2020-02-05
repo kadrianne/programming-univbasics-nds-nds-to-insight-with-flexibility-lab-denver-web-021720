078b2058a66713a014e9bffc45ff998bd1c53e80
Kristine Du <kristine.a.du@gmail.com>
2020-02-05 01:13:51 +0000

    Automatically backed up by Learn

--- scratch.rb
+++ scratch.rb
@@ -9,10 +9,13 @@ titles = [
             ]
             }
           ] 
-index = 0
-while title_index < titles.length do
+title_index = 0
+    inner_array = []
+    director_titles = {}
+    while title_index < titles.length do
       director_titles[:name] = titles[:name]
       director_titles[:title] = titles[:movies][title_index][:title]
       inner_array << director_titles
       title_index += 1
     end
+inner_array
