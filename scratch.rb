ddd1b7c03aa0e994f3bf038874e6720bfa34c805
Kristine Du <kristine.a.du@gmail.com>
2020-02-05 01:13:14 +0000

    Automatically backed up by Learn

--- scratch.rb
+++ scratch.rb
@@ -1,5 +1,15 @@
-titles = 
-
+titles = [
+            { :name => "Byron Poodle", :movies => [
+              { :title => "At the park" },
+              { :title => "On the couch" },
+            ]
+            },
+            { :name => "Nancy Drew", :movies => [
+              { :title => "Biting" },
+            ]
+            }
+          ] 
+index = 0
 while title_index < titles.length do
       director_titles[:name] = titles[:name]
       director_titles[:title] = titles[:movies][title_index][:title]
