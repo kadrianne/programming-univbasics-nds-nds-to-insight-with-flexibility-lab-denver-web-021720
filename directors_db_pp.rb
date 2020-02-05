54b93b175839fce495241e2f7997a6dd15b7f526
Kristine Du <kristine.a.du@gmail.com>
2020-02-05 01:43:44 +0000

    Automatically backed up by Learn

--- directors_db_pp.rb
+++ directors_db_pp.rb
@@ -286,6 +286,5 @@ end
 
   a_o_a_movies_with_director_names = movies_with_directors_set(db)
   movies_with_director_names = flatten_a_o_a(a_o_a_movies_with_director_names)
-  return gross_per_studio(movies_with_director_names)
-end
+
 pp movies_with_directors_set(db)
